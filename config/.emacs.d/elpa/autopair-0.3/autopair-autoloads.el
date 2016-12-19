96b01e1091d48d2da63831e8316f90db4a6ed060
Rohit <rohit.banerjee23@gmail.com>
2016-12-18 21:28:50 -0500

    Adding emacs.d

---
+++ config/.emacs.d/elpa/autopair-0.3/autopair-autoloads.el
@@ -0,0 +1,16 @@
+;;; autopair-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("autopair-pkg.el" "autopair.el") (22603
+;;;;;;  18470 366896 965000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; autopair-autoloads.el ends here
